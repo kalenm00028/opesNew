fda03b8ee834634e4f47b42e94114a1389e7f337
axeldimaria <antwari@fandm.edu>
2017-09-14 00:21:28 -0700

    Displaying resources from Firebase

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var config = {
-	    apiKey: "", //removed
+	    apiKey: "AIzaSyDiFSo7Lntffp1h-XPA-w25RIEsrhwHaT0", //removed
 	    authDomain: "opes-e9bf7.firebaseapp.com",
 	    databaseURL: "https://opes-e9bf7.firebaseio.com",
 	    projectId: "opes-e9bf7",
@@ -48,11 +48,74 @@ var isAuth = function() {
 		if (user) {
 			return true;
 		} else {
-			window.location = "file:///C:/Users/mccrea_ka/Desktop/opes/index.html";
+			//window.location = "file:///C:/Users/mccrea_ka/Desktop/opes/index.html";
+			return true;
 		}
 	});
 	const btnSignOut = document.getElementById("signOutButton");
 	btnSignOut.addEventListener('click', e => {
 		firebase.auth().signOut();
 	});
+}
+
+var displayResources = function(){
+
+	var kevinDiv = document.getElementById('kevin');
+
+	var resourceRef = firebase.database().ref().child("Kevin Resources");
+	resourceRef.on('value', function(snapshot){
+
+		var table = document.createElement('table');
+		
+		//var list = document.createElement('ul');
+		snapshot.forEach(function(childSnapshot) {
+		    var childKey = childSnapshot.key;
+		    var childData = childSnapshot.val();
+		    console.log(childData);
+
+		    
+		    var tr = document.createElement('tr');   
+
+		    var tag_td = document.createElement('td');
+		    //var category_td = document.createElement('td');
+		    var education_td = document.createElement('td');
+		    var languages_td = document.createElement('td');
+		    var profession_td = document.createElement('td');
+		    var regions_td = document.createElement('td');
+		    var skillset_td = document.createElement('td');
+
+		    var text1 = document.createTextNode(childData.tag);
+		    //var text2 = document.createTextNode('Text2');
+		    var text3 = document.createTextNode(childData.education);
+		    var text4 = document.createTextNode(childData.languages);
+		    var text5 = document.createTextNode(childData.profession);
+		    var text6 = document.createTextNode(childData.regions);
+		    var text7 = document.createTextNode(childData.skillset);
+			
+
+		    tag_td.appendChild(text1);
+		    //category_td.appendChild(text2);
+		    education_td.appendChild(text3);
+		    languages_td.appendChild(text4);
+		    profession_td.appendChild(text5);
+		    regions_td.appendChild(text6);
+		    skillset_td.appendChild(text7);
+
+		    tr.appendChild(tag_td);
+		    //tr.appendChild(td2);
+		    tr.appendChild(education_td);
+		    tr.appendChild(languages_td);
+		    tr.appendChild(profession_td);
+		    tr.appendChild(regions_td);
+		    tr.appendChild(skillset_td);
+
+		    table.append(tr);
+		    
+	  	});
+
+		// Add the contents of Things:
+		kevinDiv.appendChild(table);
+
+	});
+
 }
