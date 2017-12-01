0ada075dcbe7fbbcb403ac7ec46aea2605f8c9b0
McCrea <Kalen.McCrea@leg.wa.gov>
2017-12-01 14:12:49 -0800

    Pushing updates to new branch

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var config = {
-	    apiKey: "", //removed
+	    apiKey: "AIzaSyDiFSo7Lntffp1h-XPA-w25RIEsrhwHaT0",
 	    authDomain: "opes-e9bf7.firebaseapp.com",
 	    databaseURL: "https://opes-e9bf7.firebaseio.com",
 	    projectId: "opes-e9bf7",
@@ -13,31 +13,13 @@ var indexLoad = function() {
 	firebase.auth().onAuthStateChanged(function(user) {
 		if (user) {
 			window.location = "file:///C:/Users/mccrea_ka/Desktop/opes/mission.html"
-		} else {
-			userAuth();
 		}
 	});
 }
 
-var userAuth = function() {
-	const txtEmail = document.getElementById('emailForm');
-	const txtPassword = document.getElementById('passForm');
-	const btnLogin = document.getElementById('btnLogin');
-	btnLogin.addEventListener('click', e => {
-		const email = txtEmail.value;
-		const pass = txtPassword.value;
-		const auth = firebase.auth();
-		const promise = auth.signInWithEmailAndPassword(email, pass);
-		promise.catch(e => console.log(e.message));
-	});
-}
-
 var signIn = function() {
-	const txtEmail = document.getElementById('emailForm');
-	const txtPassword = document.getElementById('passForm');
-	const btnLogin = document.getElementById('btnLogin');
-	const email = txtEmail.value;
-	const pass = txtPassword.value;
+	const email = document.getElementById('emailForm').value;
+	const pass = document.getElementById('passForm').value;
 	const auth = firebase.auth();
 	const promise = auth.signInWithEmailAndPassword(email, pass);
 	promise.catch(e => console.log(e.message));
@@ -54,5 +36,15 @@ var isAuth = function() {
 	const btnSignOut = document.getElementById("signOutButton");
 	btnSignOut.addEventListener('click', e => {
 		firebase.auth().signOut();
+	});
+}
+
+//This is how I removed values. Keeping it here so I don't forget.
+var removeValues = function() {
+	var ref = firebase.database().ref();
+	ref.on("value",function(snapshot) {
+	for(var i=1;i<=160;i++) {
+		ref.child(i).remove();
+		}
 	});
 }
